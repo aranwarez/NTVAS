221ffb61cdfd99fc2faa4517aa1df5952d30c762
Aran Shahi <aranwarez@gmail.com>
2019-08-07 15:51:30 +0545

    Excel Import, Post, Added XLSX library.

--- src/main/java/com/vas/controller/ImpNtSpController.java
+++ src/main/java/com/vas/controller/ImpNtSpController.java
@@ -7,12 +7,21 @@ package com.vas.controller;
 
 import com.dao.CommonDateDao;
 import com.dao.ImpNtSpDao;
+import com.dao.PackageDao;
 import com.dao.VASServiceDao;
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
 import java.sql.SQLException;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 import javax.servlet.http.HttpSession;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
@@ -28,44 +37,80 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class ImpNtSpController {
 
-    private static final Logger logger = LoggerFactory.getLogger(ImpNtSpController.class);
+	private static final Logger logger = LoggerFactory.getLogger(ImpNtSpController.class);
 
-    @RequestMapping(value = "/impntsp/list", method = RequestMethod.GET)
-    public String impntsplist(String IMP_YEAR, String IMP_PERIOD, String IMP_MONTH, String SERVICE_CODE,
-            String NT_SP, String POST_FLAG, Locale locale, Model model) throws SQLException {
-        logger.info("Getting ImpMOMT List", locale);
-        ImpNtSpDao dao = new ImpNtSpDao();
-        List<Map<String, Object>> list = null;
-        try {
-            list = dao.getImpNtSpList(IMP_YEAR, IMP_PERIOD, IMP_MONTH, SERVICE_CODE, NT_SP, POST_FLAG);
-        } catch (SQLException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-        model.addAttribute("fx", "Excel MO MT of NT/SP List");
-        model.addAttribute("data_list", list);
-        VASServiceDao VASSER = new VASServiceDao();
-        model.addAttribute("VASSer_list", VASSER.getVasServiceList());
-        CommonDateDao DAT = new CommonDateDao();
-        model.addAttribute("Date_list", DAT.getDateList());       
-        CommonDateDao mon = new CommonDateDao();
-        model.addAttribute("Mon_list",mon.getNepMonthList());
-        return "impntsp/list";
-    }
+	@RequestMapping(value = "/impntsp/list", method = RequestMethod.GET)
+	public String impntsplist(String IMP_YEAR, String IMP_PERIOD, String IMP_MONTH, String SERVICE_CODE, String NT_SP,
+			String POST_FLAG, Locale locale, Model model) throws SQLException {
+		logger.info("Getting ImpMOMT List", locale);
+		ImpNtSpDao dao = new ImpNtSpDao();
+		List<Map<String, Object>> list = null;
+		try {
+			list = dao.getImpNtSpList(IMP_YEAR, IMP_PERIOD, IMP_MONTH, SERVICE_CODE, NT_SP, POST_FLAG);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		model.addAttribute("fx", "Excel MO MT of NT/SP List");
+		model.addAttribute("data_list", list);
+		VASServiceDao VASSER = new VASServiceDao();
+		model.addAttribute("VASSer_list", VASSER.getVasServiceList());
+		CommonDateDao DAT = new CommonDateDao();
+		model.addAttribute("Date_list", DAT.getDateList());
+		CommonDateDao mon = new CommonDateDao();
+		model.addAttribute("Mon_list", mon.getNepMonthList());
+		return "impntsp/list";
+	}
 
-    // getting list of all SP target
-    @ResponseBody
-    @RequestMapping(value = "/impntsp/getImpNtSplist", method = RequestMethod.GET)
-    public List<Map<String, Object>> getImpNtSplist(String IMP_YEAR, String IMP_PERIOD, String IMP_MONTH, String SERVICE_CODE,
-            String NT_SP, String POST_FLAG, Locale locale, Model model,
-            HttpSession session) throws SQLException {
-        ImpNtSpDao dao = new ImpNtSpDao();
-        return dao.getImpNtSpList(IMP_YEAR, IMP_PERIOD, IMP_MONTH, SERVICE_CODE, NT_SP, POST_FLAG);
-    }
+	// getting list of all SP target
+	@ResponseBody
+	@RequestMapping(value = "/impntsp/getImpNtSplist", method = RequestMethod.GET)
+	public List<Map<String, Object>> getImpNtSplist(String IMP_YEAR, String IMP_PERIOD, String IMP_MONTH,
+			String SERVICE_CODE, String NT_SP, String POST_FLAG, Locale locale, Model model, HttpSession session)
+			throws SQLException {
+		ImpNtSpDao dao = new ImpNtSpDao();
+		return dao.getImpNtSpList(IMP_YEAR, IMP_PERIOD, IMP_MONTH, SERVICE_CODE, NT_SP, POST_FLAG);
+	}
 
-    @RequestMapping(method = RequestMethod.GET, value = "dialogimpntsp")
-    public String dialogcp(Model model, Locale locale) {
-        return "impntsp/impntspdialog";
-    }
+	@RequestMapping(method = RequestMethod.GET, value = "dialogimpntsp")
+	public String dialogcp(Model model, Locale locale) {
+		return "impntsp/impntspdialog";
+	}
+
+	// posting Excel data into db
+	@RequestMapping(value = "/impntsp/postExcelData", method = RequestMethod.POST)
+	@ResponseBody
+	public String saveExcelData(String JSONarrayList, String Filename, Model model, Locale locale, String IMP_YEAR,
+			String Period, String IMP_MONTH, String Services, String NT_SP, String SERVICE_CODE, String IMP_PERIOD)
+			throws JsonParseException, JsonMappingException, IOException, SQLException {
+
+		logger.info("Saving Excel data in TMP table in DB {}.", locale);
+		String USER = "NEpal";
+		ObjectMapper mapper = new ObjectMapper();
+
+//		TypeReference<Map<String, Object>> typeRef = new TypeReference<Map<String, Object>>() {
+//		};
+//
+//		List<Map<String, Object>> list = mapper.readValue(JSONarrayList, typeRef);
+
+		ImpNtSpDao dao = new ImpNtSpDao();
+		List<Map<String, Object>> myObjects = mapper.readValue(JSONarrayList,
+				new TypeReference<List<Map<String, Object>>>() {
+				});
+		String msg = dao.saveExcelData(myObjects, Filename, IMP_YEAR, Period, IMP_MONTH, Services, NT_SP, SERVICE_CODE,
+				IMP_PERIOD);
+		return msg;
+	}
+
+	@RequestMapping(value = "/impntsp/postExcelDataTransaction", method = RequestMethod.POST)
+	@ResponseBody
+	public String postExcelDataTrans(Model model, Locale locale, String IMP_YEAR, String Period, String IMP_MONTH,
+			String Services, String NT_SP, String SERVICE_CODE, String IMP_PERIOD) throws SQLException {
+		logger.info("Saving Excel data in TMP table in DB {}.", locale);
+		String USER = "NEpal";
+		ImpNtSpDao dao = new ImpNtSpDao();
+		String msg = dao.postExcelImportData(IMP_YEAR, IMP_PERIOD, IMP_MONTH, Services, NT_SP, USER);
+		return msg;
+	}
 
 }
