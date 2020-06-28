b1f599093f6209a930cf31d730acadac660f0571
nabin <nabin@DESKTOP-9R845UF>
2020-06-28 14:38:02 +0545

    vasReport2020jun28

--- src/main/java/com/vas/controller/ReportMenuController.java
+++ src/main/java/com/vas/controller/ReportMenuController.java
@@ -97,7 +97,7 @@ public class ReportMenuController {
             return "/home";
         }
         model.addAttribute("MENU_ACCESS", menuaccess);
-        model.addAttribute("fx", "Report for SP Revenue");
+        model.addAttribute("fx", "Report for Overall VAS Revenue");
 
         // getting list of paramater required
         SpDao sp = new SpDao();
@@ -117,6 +117,41 @@ public class ReportMenuController {
         model.addAttribute("Mon_list", mon.getNepMonthList());
 
         return "report/sprevenue";
+
+    }
+    
+    @RequestMapping(value = "/report/spsmsrevenue", method = RequestMethod.GET)
+    public String menusmsrevenueList(Locale locale, Model model, HttpServletRequest request)
+            throws Exception {
+        String url = request.getServletPath();
+
+        UserInformationModel user = (UserInformationModel) request.getSession().getAttribute("UserList");
+        MenuAccess menuaccess = CommonMenuDao.checkAccess(user.getROLE_CODE(), ".." + url);
+        if (menuaccess == null || menuaccess.getADD_FLAG().equals("N")) {
+            model.addAttribute("fx", "Unauthorized Page for this role!!");
+            return "/home";
+        }
+        model.addAttribute("MENU_ACCESS", menuaccess);
+        model.addAttribute("fx", "Report for SMS Revenue");
+
+        // getting list of paramater required
+        SpDao sp = new SpDao();
+        model.addAttribute("Sp_list", sp.getSpList());
+
+        VASServiceDao VASSER = new VASServiceDao();
+        model.addAttribute("VASSer_list", VASSER.getVasServiceList());
+
+        ItemDao ITEM = new ItemDao();
+        model.addAttribute("ITEMwotax_list", ITEM.getItemListwotax());
+
+        BankDao bank = new BankDao();
+        CommonDateDao DAT = new CommonDateDao();
+        model.addAttribute("Date_list", DAT.getDateList());
+
+        CommonDateDao mon = new CommonDateDao();
+        model.addAttribute("Mon_list", mon.getNepMonthList());
+
+        return "report/spsmsrevenue";
 
     }
 
